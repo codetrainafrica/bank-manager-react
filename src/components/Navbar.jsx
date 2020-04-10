72b3ba5669b09eb81aa311bc496d89391fcf6661
zak <codedpills@gmail.com>
2020-04-10 14:04:09 +0000

    chore: add routes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,50 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <NavLink className="navbar-item" to="/">
-          ACCOUNT MANAGER
-        </NavLink>
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <NavLink className="navbar-item">Add account</NavLink>
-          <NavLink className="navbar-item">See accounts</NavLink>
+    <div className="container">
+      <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <NavLink className="navbar-item" to="/">
+            &nbsp;&nbsp; <strong>ACCOUNT MANAGER</strong>
+          </NavLink>
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-      </div>
-      <div className="navbar-end">
-        <div className="navbar-item">
-          <div className="buttons">
-            <NavLink className="button is-primary" to="/signup">
-              <strong>Sign up</strong>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <NavLink className="navbar-item" to="/add-account">
+              Add account
             </NavLink>
-            <NavLink className="button is-light" to="/login">Log in</NavLink>
+            <NavLink className="navbar-item" to="/">
+              See accounts
+            </NavLink>
           </div>
         </div>
-      </div>
-    </nav>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <NavLink className="button is-primary" to="/signup">
+                <strong>Sign up</strong>
+              </NavLink>
+              <NavLink className="button is-light" to="/login">
+                Log in
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
