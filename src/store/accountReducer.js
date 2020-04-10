906f66767d1a937270117e80a7a671e4509c8ec6
zak <codedpills@gmail.com>
2020-04-10 17:51:37 +0000

    chore: add functionality to edit account

--- src/store/accountReducer.js
+++ src/store/accountReducer.js
@@ -37,7 +37,22 @@ const accountReducer = (state = initialState, action) => {
       const filteredAccounts = state.accounts.filter(
         (account) => account.id !== action.id
       );
-      return {...state, accounts: filteredAccounts}
+      return { ...state, accounts: filteredAccounts };
+    case "EDIT_ACCOUNT":
+      const editedAccounts = state.accounts.map((account) => {
+        if (account.id === action.id) {
+          return {
+            id: account.id,
+            account_name: action.account.account_name,
+            account_number: action.account.account_number,
+            account_type: action.account.account_type,
+            bank_name: action.account.bank_name,
+            bank_branch: action.account.bank_branch,
+          };
+        }
+        return account;
+      });
+      return { ...state, accounts: editedAccounts };
     default:
       return state;
   }
