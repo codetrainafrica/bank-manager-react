533b3af1e1a8586ca8ccc4544076826d21338f24
zak <codedpills@gmail.com>
2020-04-10 16:27:34 +0000

    chore: wrap tbody around table content

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -16,27 +16,31 @@ const AccountInfo = ({
   return (
     <div className="card">
       <header className="card-header">
-        <p className="card-header-title has-text-danger">{account_type} Account</p>
+        <p className="card-header-title has-text-danger">
+          {account_type} Account
+        </p>
       </header>
       <div className="card-content">
         <div className="content">
           <table className="table is-striped is-hoverable">
-            <tr>
-              <th>Bank</th>
-              <td>{bank_name}</td>
-            </tr>
-            <tr>
-              <th>Branch</th>
-              <td>{bank_branch}</td>
-            </tr>
-            <tr>
-              <th>Account name</th>
-              <td>{account_name}</td>
-            </tr>
-            <tr>
-              <th>Account number</th>
-              <td>{account_number}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <th>Bank</th>
+                <td>{bank_name}</td>
+              </tr>
+              <tr>
+                <th>Branch</th>
+                <td>{bank_branch}</td>
+              </tr>
+              <tr>
+                <th>Account name</th>
+                <td>{account_name}</td>
+              </tr>
+              <tr>
+                <th>Account number</th>
+                <td>{account_number}</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
