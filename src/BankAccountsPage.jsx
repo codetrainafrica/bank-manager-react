c891dd3b0c1d758d198c2dafc52b9ef58f98d856
zak <codedpills@gmail.com>
2020-04-10 15:24:31 +0000

    feat: display bank accounts

---
+++ src/BankAccountsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BankAccounts from './components/BankAccounts';
+
+
+const BankAccountsPage = () => {
+    return (
+        <div>
+            <BankAccounts />
+        </div>
+    );
+}
+
+export default BankAccountsPage;
