999f3b1c1237c77bacb03aae37d7fe72451574ae
zak <codedpills@gmail.com>
2020-04-11 01:25:51 +0000

    chore: create Signup component

---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<h1>Signup</h1>)
+    }
+}
+
+export default Signup;
